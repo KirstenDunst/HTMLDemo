a39b300a3110386f091d1e6ef7de6bb6717417f3
KirstenDunst <cao_shixin@yahoo.com>
2017-06-26 16:55:30 +0800

    add

--- js/Day.js
+++
@@ -1,6 +0,0 @@
-function changeIframeSRC (id,newSRC) {
-	var element = document.getElementById(id);
-	element.src = newSRC;
-	element.Window.reload();
-	element.Window.body.style.backgroundColor = "aliceblue";
-}
