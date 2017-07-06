f0c5ca2c62169e45990265828d367a7abee9f07f
KirstenDunst <cao_shixin@yahoo.com>
2017-07-06 17:33:22 +0800

    Math函数里面的几个常用的方法调用

---
+++ js/Math对象.js
@@ -0,0 +1,23 @@
+function MyClick(){
+	
+//	alert(Math.PI);//圆周率的值。
+
+//获取最大值
+//alert(Math.max(10,22,45));//结果45
+
+//获取最小值
+//alert(Math.min(10,22,45));//结果10
+
+//四舍五入
+//alert(Math.round(12.56));//结果13
+
+//向上舍入
+//alert(Math.ceil(12.9));//结果13
+
+//向下舍入
+//alert(Math.floor(12.9));//结果12
+
+//获取0～1之间的随机数（不包括0和1）
+alert(Math.random());
+
+}
